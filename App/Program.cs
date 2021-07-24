9f535a9067249e7e13cd552a956ad00fe84e6514
Laurent Michel <softozor@gmail.com>
2021-07-24 16:44:29 +0200

    add application code + docker stuff

---
+++ App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Ninject;
+using Softozor.Publishodon;
+
+namespace App
+{
+    internal class Program
+    {
+        private static async Task<int> Main(string[] args)
+        {
+            using var kernel = KernelFactory.Create();
+            var bootstrapper = kernel.Get<Bootstrapper>();
+            var result = await bootstrapper.RunAsync(args);
+            return (int) result;
+        }
+    }
+}
